65e63f7451284775dd43b58d4b2d8d8c6b7ac731
Alex <alexandrulapshin@gmail.com>
2022-09-24 19:55:30 -0400

    Add an exception handler to catch any exception

--- spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerRestExceptionHandler.java
+++ spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerRestExceptionHandler.java
@@ -20,6 +20,16 @@ public class CustomerRestExceptionHandler {
 		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);	
 	}
 	
-	// Add another exception to catch any exception(catch all)
 	
+	// Add another exception handler to catch any exception(catch all)
+	@ExceptionHandler
+	public ResponseEntity<CustomerErrorResponse> handleException(Exception exc){
+	
+		// create CustomerErrorResponse
+		CustomerErrorResponse error = new CustomerErrorResponse(HttpStatus.BAD_REQUEST.value(),
+																exc.getMessage(),
+																System.currentTimeMillis());
+		
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}
 }
