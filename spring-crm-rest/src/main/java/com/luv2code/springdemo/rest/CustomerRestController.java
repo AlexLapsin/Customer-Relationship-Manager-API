54167c405badc1ffeb7a8aeb9e90a463c3028804
Alex <alexandrulapshin@gmail.com>
2022-09-24 20:34:02 -0400

    Add mapping for PUT /customers - update an existing customer

--- spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,12 +22,14 @@ public class CustomerRestController {
 	@Autowired
 	private CustomerService customerService;
 	
+	
 	// add mapping for GET /customers
 	@GetMapping("/customers")
 	public List<Customer> getCustomers(){
 		return customerService.getCustomers();
 	}
 
+	
 	// add mapping for GET /customers/{customerId}
 	@GetMapping("/customers/{customerId}")
 	public Customer getCustomer(@PathVariable int customerId){
@@ -40,6 +43,7 @@ public class CustomerRestController {
 		return theCustomer; 
 	}
 	
+	
 	// add mapping for POST /customers - add new customer
 	@PostMapping("/customers")
 	public Customer addCustomer(@RequestBody Customer theCustomer) {
@@ -52,7 +56,15 @@ public class CustomerRestController {
 		
 		return theCustomer;
 	}
+
 	
-	
+	// add mapping for PUT /customers - update an existing customer
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer theCustomer) {
+		
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+	}
 	
 }
